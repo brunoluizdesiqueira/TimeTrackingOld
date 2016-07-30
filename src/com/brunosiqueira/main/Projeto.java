9bbe6a67da0faa6cec779365dfc417234041ec14
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2016-07-30 16:41:22 -0300

    subindo projeto.java

---
+++ src/com/brunosiqueira/main/Projeto.java
@@ -0,0 +1,15 @@
+package com.brunosiqueira.main;
+
+public class Projeto {
+	String nome;
+	String responsavel;
+	String cliente;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	
+	}
+	
+}
