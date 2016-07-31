b8521dd2daaabd5246387857bbe941ed4adaec4d
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2016-07-31 10:37:43 -0300

    Incluindo novas classes

---
+++ src/br/com/brunosiqueira/modelo/Cliente.java
@@ -0,0 +1,5 @@
+package br.com.brunosiqueira.modelo;
+
+public class Cliente extends Pessoa {
+
+}
---
+++ src/br/com/brunosiqueira/modelo/Membro.java
@@ -0,0 +1,8 @@
+package br.com.brunosiqueira.modelo;
+
+public class Membro extends Pessoa {
+	String email;
+	String senha;
+	String login;
+
+}
