c4a9765f4c33ec9e54250c859366d678cc54e2fc
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2016-08-28 21:56:40 -0300

    teste

--- src/br/com/brunosiqueira/modelo/Membro.java
+++ src/br/com/brunosiqueira/modelo/Membro.java
@@ -1,7 +1,26 @@
 package br.com.brunosiqueira.modelo;
 
 public class Membro extends Pessoa {
-	String email;
-	String senha;
-	String login;
+	private String email;
+	private String senha;
+	private String login;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}	
 }
