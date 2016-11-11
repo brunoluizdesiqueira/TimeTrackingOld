5247a71cdd0670d18e8e3097bc84854842267a48
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2016-11-11 00:57:00 -0200

    fd

--- src/br/com/brunosiqueira/modelo/TimeTracking.java
+++ src/br/com/brunosiqueira/modelo/TimeTracking.java
@@ -8,27 +8,22 @@ public class TimeTracking {
 	private List<TimeLine> timeLine;
 	
 	public TimeTracking() {
-		super();
+		super();	
 	}
 		
 	public long getTempoTotal() {
+		this.timeLine.forEach(t -> t.getInstante());
 		return tempoTotal;
-	}
-	
-	public void setTempoTotal(long tempoTotal) {
-		this.tempoTotal = tempoTotal;
 	}
 	
 	public Integer getId() {
 		return id;
 	}
 
-	
 	public List<TimeLine> getTimeLine() {
 		return timeLine;
 	}
 
-	
 	public void setTimeLine(List<TimeLine> timeLine) {
 		this.timeLine = timeLine;
 	}
