b2db912d7818aa9144c4e651dfe6803eed8f859c
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2016-11-13 22:26:16 -0200

    Comentarios atualizados

--- src/br/com/brunosiqueira/programa/Running.java
+++ src/br/com/brunosiqueira/programa/Running.java
@@ -14,13 +14,11 @@ public class Running {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		// Cadastrando um novo colaborador
+		// Cadastrando colaboradores
 		Colaborador bruno = new Colaborador("Bruno", StatusPessoa.Ativo, "brunoluizdesiqueira@gmail.com", "123456", "bruno");		   
-		
-		// Cadastrando um novo colaborador
 		Colaborador amanda = new Colaborador("Amanda", StatusPessoa.Ativo, "amanda.manso@icloud.com", "654321", "amanda");		   
 		
-		// Cria um novo projeto
+		// Criando um projeto
 		Projeto erp = new Projeto("Bimer Project");
 
 		// Criando um cliente-projeto
@@ -29,11 +27,9 @@ public class Running {
 		// Seta o cliente do projeto
 		erp.setCliente(alterdata);	
 		
+		// Criando tarefas
 		Tarefa t1 = new Tarefa("Tarefa de criacao do Emissor MDF-e",bruno, amanda);
-		
-		// Criando uma nova tarefa
-		Tarefa t2 = new Tarefa("Tarefa de criacao do Emissor CT-e", amanda, bruno);
-		
+		Tarefa t2 = new Tarefa("Tarefa de criacao do Emissor CT-e", amanda, bruno);	
 		Tarefa t3 = new Tarefa("Tarefa de criacao do Emissor NF-e", bruno, bruno);
 		
 		// Acessa um projeto associado ao cliente e inclui uma nova tarefa
@@ -41,7 +37,7 @@ public class Running {
 		alterdata.getProjeto(erp).setTarefas(t2);
 		alterdata.getProjeto(erp).setTarefas(t3);
 		
-		// Imprimindo no console o nome do projeto associado ao cliente e as tarefas likadas a este projeto
+		// Imprimindo no console o nome do projeto associado ao cliente e as tarefas ligadas a este projeto
 		System.out.println(alterdata.getProjeto(erp).getNome().toString() + " " + alterdata.getProjeto(erp).getAllDescricaoTarefas());
 		
 		Tarefa t;
@@ -54,21 +50,27 @@ public class Running {
 		// Espera 30 segundos
 		TimeUnit.SECONDS.sleep(2);
 		
+		// Pausa a tarefa 
 		t.getTimeTracking().registrar(TipoTransicao.Pausado, "Parando para almoço", bruno);
 		
 		// Espera 10 segundos
 		TimeUnit.SECONDS.sleep(30);
-			
+		
+		// Reiniciando a tarefa
 		t.getTimeTracking().registrar(TipoTransicao.Reiniciado, "Retornando do almoço", bruno);
 		
 		// Espera 10 segundos
 		TimeUnit.SECONDS.sleep(40);
-					
+		
+		// Finaliza a tarefa
 		t.getTimeTracking().registrar(TipoTransicao.Finalizado, "Tarefa concluída", bruno);			
+		
+		// Imprimindo todas os tempos
 		t.getTimeTracking().getTimeLine().forEach(l -> System.out.println(l.getTransicao() + " " + l.getInstante()));
 		
 		Duration d = t.getTimeTracking().getTempoTotal();
 		
+		// Imprimindo o total do tempo gasto
 		System.out.printf("%s hora(s), %s minuto(s) e %s segundo(s)",
 				d.toHours(), d.toMinutes(), d.getSeconds());
 		
